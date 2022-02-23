a1a322e57f74b09feb453a3e57bb7f1a3605bd28
Shuv0Khan <naweemshuvo@gmail.com>
2022-02-22 23:05:52 -0500

    common 25

--- src/main/java/common25/ArrayProblems.java
+++ src/main/java/common25/ArrayProblems.java
@@ -1,6 +1,7 @@
 package common25;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Source - https://www.educative.io/blog/microsoft-interview-coding-questions
@@ -18,6 +19,8 @@ import java.util.Arrays;
  *      4. Make sure to check the negative cases, like what will happen
  *      if the input doesn't have any missing value? or if no duplicates?
  *
+ * problem 2:
+ *      1. Integer.MIN_VALUE doesn't become positive even if we do Math.abs
  */
 public class ArrayProblems {
     /**
@@ -88,6 +91,141 @@ public class ArrayProblems {
         ret[0] = (int) (seriesSum - sum + ret[1]); //missing number
 
         return ret;
+    }
+
+
+    /**
+     * <b>2. Determine if the sum of two integers is equal to a given value
+     * <br>
+     * Problem Statement:</b> Given an array of integers and a value,
+     * determine if there are any two integers in the array whose sum
+     * is equal to the given value.
+     * Return true if the sum exists, and false if it does not.
+     * @param nums array of integers
+     * @param target the value that is equal to sum of any two ints of nums.
+     * @return True if present, false otherwise.
+     */
+    public static boolean sumEqualToTargetBinSearch(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return false;
+        }
+
+        Arrays.sort(nums); // may use counting sort for O(n+k) time and O(n) space.
+
+        /*
+         * shuvo - 2022-02-20, Sun, 11:8
+         * Done to avoid finding num1 in nums if target = 2*num1
+         * Done outside for loop to avoid if-else for each iteration.
+         **/
+        int num1 = nums[0];
+        int num2 = target - num1;
+        nums[0] = nums[1];
+        if (Arrays.binarySearch(nums, num2) >= 0) {
+            return true;
+        }
+        nums[0] = num1;
+
+        for (int i = 1; i < nums.length; i++) {
+            num1 = nums[i];
+            num2 = target - nums[i];
+            nums[i] = nums[i-1];
+            if (Arrays.binarySearch(nums, num2) >= 0) {
+                return true;
+            }
+            nums[i] = num1;
+        }
+        return false;
+    }
+
+    /**
+     * Alternative 1: Using Two pivots at the two ends of the
+     * sorted array instead of using binary search.
+     * @param nums array of integers
+     * @param target the value that is equal to sum of any two ints of nums.
+     * @return True if present, false otherwise.
+     */
+    public static boolean sumEqualToTargetTwoPivots(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return false;
+        }
+
+        Arrays.sort(nums);
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        for (;start<end;) {
+            long sum = nums[start] + nums[end]; // long to avoid overflow/underflow
+            if (sum == target) {
+                return true;
+            } else if (sum < target) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Alternative 3: Using Hashmap with O(n) time and O(n) space.
+     *
+     * @param nums array of integers
+     * @param target the value that is equal to sum of any two ints of nums.
+     * @return True if present, false otherwise.
+     */
+    public static boolean sumEqualToTargetHashMap(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return false;
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            int sub = target - num;
+            if (map.get(sub) != null) {
+                return true;
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Alternative 4: Using reminders with O(n) time and O(n+target) space.
+     *
+     * @param nums array of integers
+     * @param target the value that is equal to sum of any two ints of nums.
+     * @return True if present, false otherwise.
+     */
+    public static boolean sumEqualToTargetReminder(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return false;
+        }
+        target = Math.abs(target);
+        int[] remCounts = new int[target];
+        for (int num: nums) {
+            num = Math.abs(num); // Integer.MIN_VALUE doesn't change even if we do absolute.
+            if (num > 0 && num < target) {
+                remCounts[num % target]++;
+            }
+        }
+
+        for (int num1 = 1, l = (int) (Math.floor(target/2.0) + 1); num1 < l; num1++) {
+            int num2 = target - num1;
+            if (num1 == num2) {
+                if (remCounts[num1] > 1) {
+                    return true;
+                }
+            }else if (remCounts[num1] + remCounts[num2] > 1) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
--- src/test/java/common25/ArrayProblemsTest.java
+++ src/test/java/common25/ArrayProblemsTest.java
@@ -9,7 +9,7 @@ class ArrayProblemsTest {
     @Test
     @DisplayName("Find Missing Num basic tests")
     void findMissingNumBasicTests() {
-        int [] nums = new int[]{2};
+        int[] nums = new int[]{2};
 
         assertEquals(1, ArrayProblems.findMissingNum(nums));
         assertEquals(1, ArrayProblems.findMissingNumXor(nums));
@@ -34,8 +34,8 @@ class ArrayProblemsTest {
     @Test
     @DisplayName("Find Duplicate Num Basic test")
     void findMissAndDupNumBasicTest() {
-        int [] nums = new int[]{2};
-        int [] ret = new int[2];
+        int[] nums = new int[]{2};
+        int[] ret = new int[2];
         assertArrayEquals(ret, ArrayProblems.findMissAndDupNum(nums));
 
         nums = new int[]{1, 1};
@@ -68,5 +68,60 @@ class ArrayProblemsTest {
         }
         assertEquals(1, ArrayProblems.findMissingNum(nums));
         */
+    }
+
+    @Test
+    @DisplayName("Sum of two Equal to given basic test")
+    void sumOfTwoEqualToGivenBasicTest() {
+        assertFalse(ArrayProblems.sumEqualToTargetBinSearch(new int[]{2}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetBinSearch(new int[]{-1, 2}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetBinSearch(new int[]{-1, 0}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetBinSearch(new int[]{1, Integer.MAX_VALUE}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetBinSearch(new int[]{-1, Integer.MIN_VALUE}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetBinSearch(new int[]{2, 3}, 4));
+        assertTrue(ArrayProblems.sumEqualToTargetBinSearch(new int[]{-1, 2}, 1));
+        assertTrue(ArrayProblems.sumEqualToTargetBinSearch(new int[]{1, 2}, 3));
+        assertTrue(ArrayProblems.sumEqualToTargetBinSearch(new int[]{-1, -2}, -3));
+        assertTrue(ArrayProblems.sumEqualToTargetBinSearch(new int[]{1, 2, -1, 4, 0, 3}, 3));
+        assertTrue(ArrayProblems.sumEqualToTargetBinSearch(new int[]{1, 2, 1, 4, 0, -2}, 2));
+
+
+        assertFalse(ArrayProblems.sumEqualToTargetTwoPivots(new int[]{2}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetTwoPivots(new int[]{-1, 2}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetTwoPivots(new int[]{-1, 0}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetTwoPivots(new int[]{1, Integer.MAX_VALUE}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetTwoPivots(new int[]{-1, Integer.MIN_VALUE}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetTwoPivots(new int[]{2, 3}, 4));
+        assertTrue(ArrayProblems.sumEqualToTargetTwoPivots(new int[]{-1, 2}, 1));
+        assertTrue(ArrayProblems.sumEqualToTargetTwoPivots(new int[]{1, 2}, 3));
+        assertTrue(ArrayProblems.sumEqualToTargetTwoPivots(new int[]{-1, -2}, -3));
+        assertTrue(ArrayProblems.sumEqualToTargetTwoPivots(new int[]{1, 2, -1, 4, 0, 3}, 3));
+        assertTrue(ArrayProblems.sumEqualToTargetTwoPivots(new int[]{1, 2, 1, 4, 0, -2}, 2));
+
+
+        assertFalse(ArrayProblems.sumEqualToTargetHashMap(new int[]{2}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetHashMap(new int[]{-1, 2}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetHashMap(new int[]{-1, 0}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetHashMap(new int[]{1, Integer.MAX_VALUE}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetHashMap(new int[]{-1, Integer.MIN_VALUE}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetHashMap(new int[]{2, 3}, 4));
+        assertTrue(ArrayProblems.sumEqualToTargetHashMap(new int[]{-1, 2}, 1));
+        assertTrue(ArrayProblems.sumEqualToTargetHashMap(new int[]{1, 2}, 3));
+        assertTrue(ArrayProblems.sumEqualToTargetHashMap(new int[]{-1, -2}, -3));
+        assertTrue(ArrayProblems.sumEqualToTargetHashMap(new int[]{1, 2, -1, 4, 0, 3}, 3));
+        assertTrue(ArrayProblems.sumEqualToTargetHashMap(new int[]{1, 2, 1, 4, 0, -2}, 2));
+
+
+        assertFalse(ArrayProblems.sumEqualToTargetReminder(new int[]{2}, 3));
+//        assertFalse(ArrayProblems.sumEqualToTargetReminder(new int[]{-1, 2}, 3));
+//        assertFalse(ArrayProblems.sumEqualToTargetReminder(new int[]{-1, 0}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetReminder(new int[]{1, Integer.MAX_VALUE}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetReminder(new int[]{-1, Integer.MIN_VALUE}, 3));
+        assertFalse(ArrayProblems.sumEqualToTargetReminder(new int[]{2, 3}, 4));
+//        assertTrue(ArrayProblems.sumEqualToTargetReminder(new int[]{-1, 2}, 1));
+        assertTrue(ArrayProblems.sumEqualToTargetReminder(new int[]{1, 2}, 3));
+        assertTrue(ArrayProblems.sumEqualToTargetReminder(new int[]{-1, -2}, -3));
+        assertTrue(ArrayProblems.sumEqualToTargetReminder(new int[]{1, 2, 0, 3}, 3));
+        assertTrue(ArrayProblems.sumEqualToTargetReminder(new int[]{1, 2, 1, 4}, 2));
     }
 }
