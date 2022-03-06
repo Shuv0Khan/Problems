b05f16f1135ed5c91d0fa56efa94a7907f589da8
Shuv0Khan <naweemshuvo@gmail.com>
2022-03-06 00:33:29 -0500

    common 25

--- src/main/java/common25/StringProblems.java
+++ src/main/java/common25/StringProblems.java
@@ -1,6 +1,8 @@
 package common25;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Source - https://www.educative.io/blog/microsoft-interview-coding-questions
@@ -86,5 +88,81 @@ public class StringProblems {
             }
         }
         return sentence;
+    }
+
+    /**
+     * <b>11. Find all palindrome substrings
+     * <br>
+     * Problem Statement:</b> Given a string, find all non-single letter substrings that are palindromes.
+     *<br>
+     * Example:
+     *<br>
+     * An string input of "poppopo" would return "pop", "opo", "oppo", "pp", and "poppop".
+     * <br><br><b>Uses recursion to expand the tree</b>
+     * @param s
+     * @return
+     */
+    public static Set<String> recursiveFindPalindromeSubStrings(String s) {
+        if (s == null || s.length() < 2){
+            return new HashSet<>();
+        }
+        Set<String> palindromes = new HashSet<>();
+        findPalindromes(s, 0, s.length(), palindromes);
+        return palindromes;
+    }
+
+    private static void findPalindromes(String s, int start, int end, Set<String> palindromes) {
+        addIfPalindrome(s, start, end, palindromes);
+
+        if (end - start == 2) {
+            return;
+        }
+        if (start == 0 || !palindromes.contains(s.substring(start-1, end -1))) {
+            findPalindromes(s, start, end - 1, palindromes);
+        }
+        if (end == s.length()) {
+            findPalindromes(s, start + 1, end, palindromes);
+        }
+    }
+
+    private static void addIfPalindrome(String s, int start, int end, Set<String> palindromes) {
+        int strLen = end - start;
+        int i = strLen/2 + start;
+        int j = 0;
+        if (strLen % 2 == 0) {
+            j = i + 1;
+        } else {
+            j = i + 2;
+        }
+        for (i--,j--; i > start-1 && j < end; i--, j++) {
+            if (s.charAt(i) == s.charAt(j)) {
+                palindromes.add(s.substring(i, j+1));
+            }else {
+                return;
+            }
+        }
+    }
+
+    public static Set<String> centerFindPalindromeSubStrings(String s) {
+        Set<String> palindromes = new HashSet<>();
+        for (int i = 0; i < s.length() - 1; i++) {
+            expandAtCenter(s, i, i, palindromes);
+            expandAtCenter(s, i, i + 1, palindromes);
+        }
+        return palindromes;
+     }
+
+    private static void expandAtCenter(String s, int left, int right, Set<String> palindromes) {
+        /*
+         * shuvo - 2022-03-5, Sat, 17:46
+         * To avoid single letter palindromes
+         **/
+        if (left == right) {
+            left--;
+            right++;
+        }
+        for (; left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right); left--, right++) {
+            palindromes.add(s.substring(left, right + 1));
+        }
     }
 }
--- src/test/java/common25/StringProblemsTest.java
+++ src/test/java/common25/StringProblemsTest.java
@@ -3,7 +3,10 @@ package common25;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StringProblemsTest {
     @Test
@@ -26,6 +29,88 @@ class StringProblemsTest {
 
         assertEquals(reverse, new String(StringProblems.reverseSentenceByWordsWithExtraSpace(sentence.toCharArray())));
         assertEquals(reverse, new String(StringProblems.reverseSentenceByWords(sentence.toCharArray())));
+
+    }
+
+    @Test
+    @DisplayName("find all palindrome substrings basic test")
+    void findAllPalindromeSubstringsBasicTest() {
+        String s = "poppopo";
+        Set<String> palindromes = new HashSet<>(Arrays.asList(new String[]{"pop", "oppo", "pp", "poppop", "opo"}));
+        Set<String> returns = StringProblems.recursiveFindPalindromeSubStrings(s);
+        Set<String> returns2 = StringProblems.centerFindPalindromeSubStrings(s);
+
+        assertEquals(palindromes.size(), returns.size());
+        for (String p : palindromes) {
+            assertTrue(returns.contains(p));
+            assertTrue(returns2.contains(p));
+        }
+
+        s = "abcdef";
+        palindromes = new HashSet<>();
+        returns = StringProblems.recursiveFindPalindromeSubStrings(s);
+        returns2 = StringProblems.centerFindPalindromeSubStrings(s);
+
+        assertEquals(palindromes.size(), returns.size());
+        for (String p : palindromes) {
+            assertTrue(returns.contains(p));
+            assertTrue(returns2.contains(p));
+        }
+
+        s = "ab";
+        palindromes = new HashSet<>();
+        returns = StringProblems.recursiveFindPalindromeSubStrings(s);
+        returns2 = StringProblems.centerFindPalindromeSubStrings(s);
+
+        assertEquals(palindromes.size(), returns.size());
+        for (String p : palindromes) {
+            assertTrue(returns.contains(p));
+            assertTrue(returns2.contains(p));
+        }
+
+        s = "a";
+        palindromes = new HashSet<>();
+        returns = StringProblems.recursiveFindPalindromeSubStrings(s);
+        returns2 = StringProblems.centerFindPalindromeSubStrings(s);
+
+        assertEquals(palindromes.size(), returns.size());
+        for (String p : palindromes) {
+            assertTrue(returns.contains(p));
+            assertTrue(returns2.contains(p));
+        }
+
+        s = "abcdcba";
+        palindromes = new HashSet<>(Arrays.asList(new String[]{"abcdcba", "bcdcb", "cdc"}));
+        returns = StringProblems.recursiveFindPalindromeSubStrings(s);
+        returns2 = StringProblems.centerFindPalindromeSubStrings(s);
+
+        assertEquals(palindromes.size(), returns.size());
+        for (String p : palindromes) {
+            assertTrue(returns.contains(p));
+            assertTrue(returns2.contains(p));
+        }
+
+        s = "abababa";
+        palindromes = new HashSet<>(Arrays.asList(new String[]{"abababa", "babab", "aba", "ababa", "bab"}));
+        returns = StringProblems.recursiveFindPalindromeSubStrings(s);
+        returns2 = StringProblems.centerFindPalindromeSubStrings(s);
+
+        assertEquals(palindromes.size(), returns.size());
+        for (String p : palindromes) {
+            assertTrue(returns.contains(p));
+            assertTrue(returns2.contains(p));
+        }
+
+        s = "poppop";
+        palindromes = new HashSet<>(Arrays.asList(new String[]{"poppop", "oppo", "pp", "pop"}));
+        returns = StringProblems.recursiveFindPalindromeSubStrings(s);
+        returns2 = StringProblems.centerFindPalindromeSubStrings(s);
+
+        assertEquals(palindromes.size(), returns.size());
+        for (String p : palindromes) {
+            assertTrue(returns.contains(p));
+            assertTrue(returns2.contains(p));
+        }
 
     }
 
