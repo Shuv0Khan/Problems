fac32f381e1960eb04b29448cb1de83ffa9c5b1c
Shuv0Khan <naweemshuvo@gmail.com>
2022-03-16 13:20:53 -0400

    common 25

--- src/main/java/common25/MathProblems.java
+++ src/main/java/common25/MathProblems.java
@@ -1,7 +1,7 @@
 package common25;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * Source - https://www.educative.io/blog/microsoft-interview-coding-questions
@@ -11,6 +11,15 @@ import java.util.List;
  *      1. Think some more about it.
  */
 public class MathProblems {
+    /**
+     * <b>16. Find all sum combinations
+     * <br>
+     * Problem Statement:</b> Given a positive integer, target,
+     * print all possible combinations of positive integers
+     * that sum up to the target number.
+     * @param target the target sum
+     * @return list of list containing all the sum combinations
+     */
     public static ArrayList<ArrayList<Integer>> allSumCombinations(int target) {
         ArrayList<ArrayList<Integer>> output = new ArrayList<>();
         ArrayList<Integer> result = new ArrayList<>();
@@ -34,4 +43,81 @@ public class MathProblems {
             }
         }
     }
+
+    /**
+     * Print all possible permutations of n numbers from 1 to n
+     * @param n
+     */
+    public static ArrayList<String> allPermutations(int n) {
+        if (n == 1) {
+            return new ArrayList<>(Arrays.asList("1"));
+        }
+        String s = "";
+        for (int i = 1; i<=n;i++){
+            s+=i;
+        }
+        ArrayList<String> permutations = new ArrayList<>();
+        printAllPermutations(s, 0, permutations);
+        return permutations;
+    }
+
+    private static void printAllPermutations(String a, int start, ArrayList<String> permutations) {
+        if (start == a.length()-2) {
+            permutations.add(a);
+            char [] b = a.toCharArray();
+            char ch = b[start];
+            b[start] = b[start+1];
+            b[start+1] = ch;
+            permutations.add(new String(b));
+            return;
+        }
+
+        String s = a;
+        for (int i = start+1; i <= s.length(); i++) {
+            printAllPermutations(s, start+1, permutations);
+            if (i < s.length()) {
+                char[] b = s.toCharArray();
+                char ch = b[start];
+                b[start] = b[i];
+                b[i] = ch;
+                s = new String(b);
+            }
+        }
+    }
+
+    public static String kthPermutation(int n, int k) {
+        char[] a = new char[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Character.forDigit((i+1), 10);
+        }
+        return new String(getKthPerm(a, k));
+    }
+
+    private static char[] getKthPerm(char[] a, int k) {
+        if (k == 1) {
+            /*
+             * 1 based indexing.
+             **/
+            return a;
+        }
+        int fact = 1;
+        int upperLimit = 1;
+
+        for (; fact*upperLimit < k; fact*=upperLimit, upperLimit++);
+
+        int index = a.length - upperLimit;
+        int increment = fact; // lowerLimit! => where lowerLimit! < k <= upperLimit!
+        int i = 1;
+        for (int j = index + 1; ; i+=increment, j++) {
+            if (i + increment <= k) {
+                char ch = a[index];
+                a[index] = a[j];
+                a[j] = ch;
+            } else {
+                break;
+            }
+        }
+        return getKthPerm(a, k - i + 1);
+    }
+
 }
--- src/test/java/common25/MathProblemsTest.java
+++ src/test/java/common25/MathProblemsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,6 +29,56 @@ class MathProblemsTest {
             List<Integer> out = output.get(i);
             List<Integer> res = result.get(i);
             assertArrayEquals(out.toArray(), res.toArray());
+        }
+    }
+
+    @Test
+    @DisplayName("get all permutations")
+    void getAllPermutations() {
+        ArrayList<String> permutations = new ArrayList<>(Arrays.asList("1"));
+        ArrayList<String> result = MathProblems.allPermutations(1);
+        assertEquals(permutations.size(), result.size());
+        for (int i = 0; i < permutations.size(); i++) {
+            assertEquals(permutations.get(i), result.get(i));
+        }
+
+        permutations = new ArrayList<>(Arrays.asList("12", "21"));
+        result = MathProblems.allPermutations(2);
+        for (int i = 0; i < permutations.size(); i++) {
+            assertEquals(permutations.get(i), result.get(i));
+        }
+
+        permutations = new ArrayList<>(Arrays.asList("123", "132", "213", "231", "312", "321"));
+        result = MathProblems.allPermutations(3);
+        for (int i = 0; i < permutations.size(); i++) {
+            assertEquals(permutations.get(i), result.get(i));
+        }
+
+
+        permutations = new ArrayList<>(Arrays.asList(
+                "1234", "1243", "1324", "1342", "1423", "1432",
+                "2134", "2143", "2314", "2341", "2413", "2431",
+                "3124", "3142", "3214", "3241", "3412", "3421",
+                "4123", "4132", "4213", "4231", "4312", "4321"));
+        result = MathProblems.allPermutations(4);
+        for (int i = 0; i < permutations.size(); i++) {
+            assertEquals(permutations.get(i), result.get(i));
+        }
+    }
+
+    @Test
+    @DisplayName("kth permutation basic test")
+    void kthPermutationBasicTest() {
+        int n = 4;
+        ArrayList<String> permutations = new ArrayList<>(
+            Arrays.asList(
+                "1234", "1243", "1324", "1342", "1423", "1432",
+                "2134", "2143", "2314", "2341", "2413", "2431",
+                "3124", "3142", "3214", "3241", "3412", "3421",
+                "4123", "4132", "4213", "4231", "4312", "4321"));
+
+        for (int i = 0; i < permutations.size(); i++) {
+            assertEquals(permutations.get(i), MathProblems.kthPermutation(n, i + 1));
         }
     }
 }
